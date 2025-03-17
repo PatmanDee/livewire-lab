79e5013bf0fe4ec0de0fd0bc1087c8b17812369a
Patson Chidari <patsonchids@gmail.com>
2025-03-17 17:07:36 +0200

    alpinejs tings

--- resources/views/livewire/create-post.blade.php
+++ resources/views/livewire/create-post.blade.php
@@ -4,6 +4,9 @@
         <label>
             <span>Title</span>
             <input type="text" wire:model="title" placeholder="Title">
+            <small>Words:
+                <span x-text="$wire.title.split(' ').length -1"></span>
+            </small>
             @error('title')
                 <em class="text-red-500">{{ $message }}</em>
             @enderror
@@ -11,6 +14,9 @@
         <label>
             <span>Content</span>
             <textarea wire:model="content" placeholder="Content"></textarea>
+            <small>Characters:
+                <span x-text="$wire.content.length"></span>
+            </small>
             @error('content')
                 <em class="text-red-500">{{ $message }}</em>
             @enderror
