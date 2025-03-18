cc2fafe5b7b48ff832b5b27f0d37acd674c2f6d4
Patson Chidari <patsonchids@gmail.com>
2025-03-18 11:46:17 +0200

    nesting components in livewire

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,9 +4,54 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Sushi\Sushi;
 class Post extends Model
 {
-    use HasFactory;
+    use HasFactory, Sushi;
+
+    protected $rows = [
+        [
+            'id' => 1,
+            'is_archived' => false,
+            'title' => 'Fuga atque inventore aut minima totam necessitatibus qui.',
+            'content' => 'Inventore qui iusto voluptas facilis. Cumque consequatur aliquam exercitationem ut possimus beatae.',
+        ],
+        [
+            'id' => 2,
+            'is_archived' => false,
+            'title' => 'Quisquam et quia et.',
+            'content' => 'Quisquam et quia et.',
+        ],
+        [
+            'id' => 3,
+            'is_archived' => false,
+            'title' => 'Quisquam et quia et.',
+            'content' => 'Quisquam et quia et.',
+        ],
+        [
+            'id' => 4,
+            'is_archived' => false,
+            'title' => 'Quisquam et quia et.',
+            'content' => 'Quisquam et quia et.',
+        ],
+        [
+            'id' => 5,
+            'is_archived' => false,
+            'title' => 'Quisquam et quia et.',
+            'content' => 'Quisquam et quia et.',
+        ],
+        [
+            'id' => 6,
+            'is_archived' => false,
+            'title' => 'Quisquam et quia et.',
+            'content' => 'Quisquam et quia et.',
+        ],
+    ];
+    public function archive()
+    {
+        $this->is_archived = true;
+        $this->save();
+    }
 
     protected $guarded = [];
 }
--- resources/views/livewire/show-posts.blade.php
+++ resources/views/livewire/show-posts.blade.php
@@ -12,14 +12,8 @@
         </thead>
         <tbody>
             @foreach ($posts as $post)
-                <tr wire:key="post-{{ $post->id }}">
-                    <td>{{ $post->title }}</td>
-                    <td>{{ str($post->content)->words(10) }}</td>
-                    <td>
-                        <button type="button" wire:click="editPost({{ $post->id }})">Edit</button>
-                        <button type="button" wire:confirm="Are you sure you want to delete this post?" wire:click="deletePost({{ $post->id }})">Delete</button>
-                    </td>
-                </tr>
+                <livewire:post-row :key="$post->id" :$post />
+
             @endforeach
         </tbody>
     </table>
